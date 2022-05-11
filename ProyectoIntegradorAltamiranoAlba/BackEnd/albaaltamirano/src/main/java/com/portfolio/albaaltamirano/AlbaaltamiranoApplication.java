40372fe3e1bea8f9b7fea090758373cf06666dd2
alba altamirano <alba_martina@hotmail.com>
2022-05-11 20:21:53 -0300

    Cambios

---
+++ ProyectoIntegradorAltamiranoAlba/BackEnd/albaaltamirano/src/main/java/com/portfolio/albaaltamirano/AlbaaltamiranoApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.albaaltamirano;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlbaaltamiranoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlbaaltamiranoApplication.class, args);
+	}
+
+}
---
+++ ProyectoIntegradorAltamiranoAlba/BackEnd/albaaltamirano/src/test/java/com/portfolio/albaaltamirano/AlbaaltamiranoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.albaaltamirano;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlbaaltamiranoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
