5ee581e813294a52ae5531fdee5f3aa57d802f1f
alba altamirano <alba_martina@hotmail.com>
2022-05-15 18:07:08 -0300

    backend

---
+++ ProyectoIntegradorAltamiranoAlba/BackEnd/albaaltamirano/src/main/java/com/portfolio/albaaltamirano/Repository/IPersonaRepository.java
@@ -0,0 +1,11 @@
+
+package com.portfolio.albaaltamirano.Repository;
+
+import com.portfolio.albaaltamirano.Entity.Persona;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IPersonaRepository extends JpaRepository<Persona,Long> {
+    
+}
