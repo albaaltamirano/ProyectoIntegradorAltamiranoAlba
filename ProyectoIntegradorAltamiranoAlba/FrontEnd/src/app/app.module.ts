c91ce8fdde857bcb38a38415527a78f0050085f0
alba altamirano <alba_martina@hotmail.com>
2022-05-15 22:24:56 -0300

    cambios

--- ProyectoIntegradorAltamiranoAlba/FrontEnd/src/app/app.module.ts
+++ ProyectoIntegradorAltamiranoAlba/FrontEnd/src/app/app.module.ts
@@ -11,6 +11,7 @@ import  {  NgCircleProgressModule  }  from  'ng-circle-progress';
 import { SkillsComponent } from './componentes/skills/skills.component';
 import { ProyectosComponent } from './componentes/proyectos/proyectos.component';
 import { FooterComponent } from './componentes/footer/footer.component';
+import{HttpClientModule} from '@angular/common/http';
 
 
 
@@ -30,7 +31,8 @@ import { FooterComponent } from './componentes/footer/footer.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgCircleProgressModule.forRoot({})
+    NgCircleProgressModule.forRoot({}),
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
